075dccd72d3e890e0ff7afdf38574128c11dd11c
Vedant <mainvedant.vns123@gmail.com>
2018-09-23 12:14:05 +0530

    initial 2

--- deployProject/settings.py
+++ deployProject/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
